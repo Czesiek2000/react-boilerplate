43f569a225216d91a9bed7891d50c3f6e0574e97
Michał <michal.czech00@gmail.com>
2020-10-06 22:45:41 +0200

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-// Import App
-import App from './components/App';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
